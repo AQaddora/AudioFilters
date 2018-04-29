d087ffd24b6365f96c205b9506b28a3dcc2eba82
Gamer Box Studios <hem-plo@hotmail.com>
2018-04-30 00:50:17 +0300

    HiThere
    Yo yo

---
+++ Assets/Scripts/AudioFilters.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class AudioFilters : MonoBehaviour
+{
+	public AudioMixerGroup lowPass, highPass, echo, distortion;
+	public Text title, playButtonText;
+
+	private AudioSource audioSource;
+
+	void Start ()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	public void Play()
+	{
+		if(audioSource.isPlaying)
+		{
+			audioSource.Stop();
+			playButtonText.text = "Play";
+			audioSource.outputAudioMixerGroup = null;
+		}
+		else
+		{
+			title.text = audioSource.clip.name;
+			audioSource.Play();
+			playButtonText.text = "Stop";
+		}
+	}
+
+	public void LowPassOnClick()
+	{
+		audioSource.outputAudioMixerGroup = lowPass;
+	}
+
+	public void HighPassOnClick()
+	{
+		audioSource.outputAudioMixerGroup = highPass;
+	}
+
+	public void EchoOnClick()
+	{
+		audioSource.outputAudioMixerGroup = echo;
+	}
+
+	public void DistortionOnClick()
+	{
+		audioSource.outputAudioMixerGroup = distortion;
+	}
+
+	public void BackToNormalOnClick()
+	{
+		audioSource.outputAudioMixerGroup = null;
+	}
+}
